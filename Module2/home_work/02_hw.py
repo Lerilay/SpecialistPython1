44e75a660779d6d862a30aaf2c341b0c19c4da9d
Lerilay <110842462+Lerilay@users.noreply.github.com>
2022-08-09 19:26:36 +0300

    Update 02_hw.py

--- Module2/home_work/02_hw.py
+++ Module2/home_work/02_hw.py
@@ -7,4 +7,14 @@
 # коров, корова или коровы
 # Например, 1 корова, 2 коровы, 5 коров, 125 коров.
 
-# TODO: your code here
+n = int(input())
+if n >= 11 and n <= 14:
+        print(n, 'korov')
+else:
+        temp = n % 10
+        if temp == 0 or (temp >= 5 and temp <= 9):
+                print(n, 'korov')
+        if temp == 1:
+                print(n, 'korova')
+        if temp >=2 and temp <=4:
+                print(n, 'korovy')
